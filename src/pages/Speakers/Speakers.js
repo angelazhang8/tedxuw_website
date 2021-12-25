5900092c288a4ad80ab691486193859d70055d7d
xxxzhangxxx <angelazhangyr@gmail.com>
2021-12-25 18:33:39 -0500

    fill in speaker headshots and bios

--- src/pages/Speakers/Speakers.js
+++ src/pages/Speakers/Speakers.js
@@ -2,6 +2,14 @@ import React from "react";
 import SpeakerCard from "../../components/SpeakerCard/SpeakerCard";
 import { Container, ImageGroup, Divider, Button } from "semantic-ui-react";
 import speaker_john from "../../res/images/speaker_john.png";
+import thomas_lenhard from "../../res/images/thomas_lenhard.png";
+import andrew_guy from "../../res/images/andrew_guy.png";
+import anh_le from "../../res/images/anh_le.png";
+import karandeep_gill from "../../res/images/karandeep_gill.png";
+import iman_salehi from "../../res/images/iman_salehi.png";
+import chris_costa from "../../res/images/chris_costa.png";
+import tanner_philp from "../../res/images/tanner_philp.png";
+
 import down from "../../res/images/down.svg";
 import { Link } from "react-scroll";
 
@@ -83,40 +91,61 @@ const Speakers = () => (
         <StyledSmallHeader>2021 Speakers</StyledSmallHeader>
       </p>
       <SpeakerCard
-        img={speaker_john}
-        name="John Tukums"
-        talk_name="Breath 2.0: Upgrading Your Breathing Operating System"
-        description="John Tukums is the founder and CEO of DeepSubconscious.ai Inc. and the author of the book Sustaining Happiness. He followed his father and grandfather into health care, and through his initiatives, John works with clients to help them take action and seize the day. As the host of Happiness 2.02 Podcast, John interviews world-renowned executives and remarkable leaders so that listeners can apply their wisdom. Through his work and podcast, John focuses on stripping away the fear-inducing language related to breathing and helping clients experience an integrated approach to health, wellness and well-being.  John is a Fellow Founder of the inaugural World Happiness Summit in Miami. John was invited  to be a participant of Global Dialogue for Happiness and World Government Summit in Dubai and was a speaker at the 2019 Transformative Technology Conference, which is dedicated to unlocking the future of health, work, and human excellence through transformative technology."
+        img={andrew_guy}
+        name="Andrew Guy"
+        talk_name="Find the Good in Humanity"
+        description="Andrew is a MACP candidate, he has earned degrees from Southern
+        Arkansas University, dual BSc in Medical Biology, Exercise Science, MSc,
+        Master of Science in sports medicine (Kinesiology). Andrew is a
+        professionally certified educator, Professional Teacher PD facilitator,
+        and a former teacher of the year."
       />
       <SpeakerCard
-        img={speaker_john}
-        name="John Tukums"
-        talk_name="Breath 2.0: Upgrading Your Breathing Operating System"
-        description="John Tukums is the founder and CEO of DeepSubconscious.ai Inc. and the author of the book Sustaining Happiness. He followed his father and grandfather into health care, and through his initiatives, John works with clients to help them take action and seize the day. As the host of Happiness 2.02 Podcast, John interviews world-renowned executives and remarkable leaders so that listeners can apply their wisdom. Through his work and podcast, John focuses on stripping away the fear-inducing language related to breathing and helping clients experience an integrated approach to health, wellness and well-being.  John is a Fellow Founder of the inaugural World Happiness Summit in Miami. John was invited  to be a participant of Global Dialogue for Happiness and World Government Summit in Dubai and was a speaker at the 2019 Transformative Technology Conference, which is dedicated to unlocking the future of health, work, and human excellence through transformative technology."
+        img={anh_le}
+        name="Anh Le"
+        talk_name="How to Optimize Your Odds for the Best Outcome?"
+        description="As an Asian female immigrant/ international student, I want to break out of the stereotypes and believe in myself to make a difference in the world. And to make a difference, I want to understand how the world works through the lens of research, insights, and data. Having had a natural talent for science and arts since I was little, I joined science competitions to hackathons from school to university.
+        
+        I am currently a data scientist with a keen interest in developing insights. I love learning about how the world works. I believe a great data scientist must have the speech of a diplomat, the curiosity of a scientist, and a business mindset of an entrepreneur.
+         
+        With a background in Biotech, Engineering, and Finance from the University of Waterloo (both a Bachelor and a Master), my insatiable curiosity and a strong desire to make an impact beyond myself have allowed me to step outside my comfort zones.
+         
+        Outside of work, you can find me playing Pokemon Go and chess, helping out with International Blockchain Olympiad and DarkByte Research, meditation, connecting with people, and biking.
+         
+        Past events I spoke at TEDxUW 2018, Hackthe6ix 2020, Manning Publications – Data Science, Women in Data, CAASA Master of Quantitive Finance, and DataScienceGo.
+         
+        To scale an idea, you need optimized execution and good decision-making. My blog on finance, data science, and blockchain on Medium @thisisanhle (also on other socials) has over 10,000 views a month. Follow me on Medium for my insights.
+        "
       />
       <SpeakerCard
-        img={speaker_john}
-        name="John Tukums"
-        talk_name="Breath 2.0: Upgrading Your Breathing Operating System"
-        description="John Tukums is the founder and CEO of DeepSubconscious.ai Inc. and the author of the book Sustaining Happiness. He followed his father and grandfather into health care, and through his initiatives, John works with clients to help them take action and seize the day. As the host of Happiness 2.02 Podcast, John interviews world-renowned executives and remarkable leaders so that listeners can apply their wisdom. Through his work and podcast, John focuses on stripping away the fear-inducing language related to breathing and helping clients experience an integrated approach to health, wellness and well-being.  John is a Fellow Founder of the inaugural World Happiness Summit in Miami. John was invited  to be a participant of Global Dialogue for Happiness and World Government Summit in Dubai and was a speaker at the 2019 Transformative Technology Conference, which is dedicated to unlocking the future of health, work, and human excellence through transformative technology."
+        img={iman_salehi}
+        name="Iman Salehi"
+        talk_name="Can We Treat Cancer During a Pregnancy?"
+        description="Iman Salehi is a third-year student at the University of Waterloo studying Health Studies with a minor in Medical Physiology. During his co-operative education work terms, Iman’s primary focus has been on breast cancer research, with a deeper focus on the rare occurrence of breast cancer diagnosed during pregnancy. In his most recent co-op at Mount Sinai Hospital, he collaborated with a multidisciplinary care team including physicians and oncology and obstetrical nurses to develop a comprehensive course on the topic of breast cancer diagnosed during pregnancy. In October, Iman and his research team presented their research and course at the Canadian Association of Nurses in Oncology conference and won first place for the Educational category. Today, Iman and his team continue to expand on their breast cancer in pregnancy course, with the objective to add more cancer sites. The application of Iman’s and his team’s research aims to educate oncology and obstetrical health care professionals on the importance of using evidence-based medicine to shape their clinical practice."
       />
       <SpeakerCard
-        img={speaker_john}
-        name="John Tukums"
-        talk_name="Breath 2.0: Upgrading Your Breathing Operating System"
-        description="John Tukums is the founder and CEO of DeepSubconscious.ai Inc. and the author of the book Sustaining Happiness. He followed his father and grandfather into health care, and through his initiatives, John works with clients to help them take action and seize the day. As the host of Happiness 2.02 Podcast, John interviews world-renowned executives and remarkable leaders so that listeners can apply their wisdom. Through his work and podcast, John focuses on stripping away the fear-inducing language related to breathing and helping clients experience an integrated approach to health, wellness and well-being.  John is a Fellow Founder of the inaugural World Happiness Summit in Miami. John was invited  to be a participant of Global Dialogue for Happiness and World Government Summit in Dubai and was a speaker at the 2019 Transformative Technology Conference, which is dedicated to unlocking the future of health, work, and human excellence through transformative technology."
+        img={karandeep_gill}
+        name="Karandeep Gill"
+        talk_name="How to Achieve Resilience Through Self-Compassion"
+        description="Karandeep Gill is a speech communication and business undergrad student at the University of Waterloo. Last year, she was appointed as Canada’s 2020 Face of Mental Illness. She was granted this extraordinary title for her advocacy for adolescent and South Asian mental health, along with sharing her undefeatable recovery and survival journey while living with a mental illness like Schizoaffective Disorder-Bipolar Type. Her research revolves around how one can overpower traumatic experiences like extreme paranoia, vivid hallucinations, and suicidal ideation through the art of self-compassion. She will explain how being resilient doesn’t stem solely from not giving up, but rather taking care of one's mind, body, and soul during the process."
       />
       <SpeakerCard
-        img={speaker_john}
-        name="John Tukums"
-        talk_name="Breath 2.0: Upgrading Your Breathing Operating System"
-        description="John Tukums is the founder and CEO of DeepSubconscious.ai Inc. and the author of the book Sustaining Happiness. He followed his father and grandfather into health care, and through his initiatives, John works with clients to help them take action and seize the day. As the host of Happiness 2.02 Podcast, John interviews world-renowned executives and remarkable leaders so that listeners can apply their wisdom. Through his work and podcast, John focuses on stripping away the fear-inducing language related to breathing and helping clients experience an integrated approach to health, wellness and well-being.  John is a Fellow Founder of the inaugural World Happiness Summit in Miami. John was invited  to be a participant of Global Dialogue for Happiness and World Government Summit in Dubai and was a speaker at the 2019 Transformative Technology Conference, which is dedicated to unlocking the future of health, work, and human excellence through transformative technology."
+        img={thomas_lenhard}
+        name="Thomas Lenhard"
+        talk_name="Cry Like A Man: Modeling Positive Masculinity"
+        description="Lenny Lenhard is the founder of the Modern Manhood project, which works with Montreal high schoolers to positively reimagine what it means to be a man. Rather than tell boys who they should be, the program helps boys build the self-confidence and emotional literacy to express an authentic version of themselves, free from the constraints that patriarchy imposes upon us all. Operating with a core set of lesson plans, the program - run by men, for men - discusses topics ranging from body image to sex and beyond. As a recent McGill graduate, his insights are informed by his own experiences, as well as his background in sociology - a domain which has challenged him to question how people operate in systems we take for granted."
       />
       <SpeakerCard
-        img={speaker_john}
-        name="John Tukums"
-        talk_name="Breath 2.0: Upgrading Your Breathing Operating System"
-        description="John Tukums is the founder and CEO of DeepSubconscious.ai Inc. and the author of the book Sustaining Happiness. He followed his father and grandfather into health care, and through his initiatives, John works with clients to help them take action and seize the day. As the host of Happiness 2.02 Podcast, John interviews world-renowned executives and remarkable leaders so that listeners can apply their wisdom. Through his work and podcast, John focuses on stripping away the fear-inducing language related to breathing and helping clients experience an integrated approach to health, wellness and well-being.  John is a Fellow Founder of the inaugural World Happiness Summit in Miami. John was invited  to be a participant of Global Dialogue for Happiness and World Government Summit in Dubai and was a speaker at the 2019 Transformative Technology Conference, which is dedicated to unlocking the future of health, work, and human excellence through transformative technology."
+        img={chris_costa}
+        name="Chris Costa"
+        talk_name="Choose Love, Not Fear"
+        description="The founder of The Costa Group and Change in Motion. Chris has figured out how to turn the most adversity into an engine for positive change. Both for himself and his community. Throughout his life he’s experienced violence, incarceration, and addiction – and perhaps most difficult for him; he made choices which kept him away from his passion as an athlete. Now 20 years later, he’s not only overcome his past but he’s speaking out against domestic violence. He used sport to reconnect with his past, heal it and use that experience to help his local community. He is a competitive middle-distance runner who’s competed internationally for Canada as a master’s athlete and kickboxer."
+      />
+      <SpeakerCard
+        img={tanner_philp}
+        name="Tanner Philp"
+        talk_name="Enter the Metaverse"
+        description="Tanner Philp has previously held speaking positions at TEDx UW, Tech TO, Guest Lecturer at Stanford, Laurier, UW, Multiple conferences across 10+ countries"
       />
     </Container>
     <Divider style={{ height: "100px" }} />
